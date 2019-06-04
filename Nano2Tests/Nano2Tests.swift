0d6def2519f9ab58aeb1aa1a805f9553ebbe5c0e
Felipe Alexander Silva Melo <felipealexandersilvamelo@SAO-FVFX6821HV29.local>
2019-06-04 11:54:41 -0300

    Added tests

---
+++ Nano2Tests/Nano2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Nano2Tests.swift
+//  Nano2Tests
+//
+//  Created by Felipe Alexander Silva Melo on 04/06/19.
+//  Copyright © 2019 Felipe Melo. All rights reserved.
+//
+
+import XCTest
+
+class Nano2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
